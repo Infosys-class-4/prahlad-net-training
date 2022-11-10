0db193672a870fa8852a1ec84546a9c5e5ea4f9e
prahlad.shakya <shakya123prahlad@gmail.com>
2022-11-10 11:15:46 +0545

    Assignement 1

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -6,3 +6,21 @@
 
 DateTime now = DateTime.Now;
 Console.WriteLine(now);
+
+
+// Task 1
+
+string name = "Prahlad";
+byte age = 21;
+string weight = "70kg";
+bool passport = true;
+string dob = "23rd october 2001";
+
+
+// Task 2
+
+Console.WriteLine(name);
+Console.WriteLine(age);
+Console.WriteLine(weight);
+Console.WriteLine(passport);
+Console.WriteLine(dob);
