12b9f4ace6b63b2b89499354d5c254842f26c6fa
prahlad.shakya <shakya123prahlad@gmail.com>
2022-11-10 12:39:43 +0545

    Classes and Object Day5

---
+++ CSharpFundamentals/ClassesAndObjects.cs
@@ -0,0 +1,33 @@
+// C# is object oriented programming language
+
+// Procedural: C, PASCAL, BASIC, Go
+// Imperative: Go, C#, C, C++
+// Declarative: Python
+// Object Oriented: C#, Java, Python
+// Funcational: F#, Scala, Haskell, C#
+// Dynamic: JS, Ruby, Python
+// Static: C#, Java
+
+// C# is multi-paradigm language
+
+
+class Person
+{
+    public string name;
+    public byte age;
+    public string GetLegalGreeting()
+    {
+        if (age < 18)
+        {
+            return "You are under age";
+        }
+        else
+        {
+            return "You are adult now";
+        }
+    }
+
+}
+
+
+
--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -4,23 +4,35 @@
 
 //byte a = 255;
 
-DateTime now = DateTime.Now;
-Console.WriteLine(now);
+// DateTime now = DateTime.Now;
+// Console.WriteLine(now);
 
 
 // Task 1
 
-string name = "Prahlad";
-byte age = 21;
-string weight = "70kg";
-bool passport = true;
-string dob = "23rd october 2001";
+// string name = "Prahlad";
+// byte age = 21;
+// string weight = "70kg";
+// bool passport = true;
+// string dob = "23rd october 2001";
+// DateTime dob = new DateTime(2001,10,23);
 
 
 // Task 2
 
-Console.WriteLine(name);
-Console.WriteLine(age);
-Console.WriteLine(weight);
-Console.WriteLine(passport);
-Console.WriteLine(dob);
+// Console.WriteLine(name);
+// Console.WriteLine(age);
+// Console.WriteLine(weight);
+// Console.WriteLine(passport);
+// Console.WriteLine(dob);
+
+
+
+Person person1 = new();
+person1.name = "Sitaram";
+person1.age = 8;
+
+string message = person1.GetLegalGreeting();
+Console.WriteLine(message);
+
+
